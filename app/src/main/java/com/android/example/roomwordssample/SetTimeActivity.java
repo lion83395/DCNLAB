d330b888c9aeeec3ce705bc24be523c443137320
old-mac-888 <38876972+old-mac-888@users.noreply.github.com>
2019-06-15 15:28:42 +0800

    .

--- app/src/main/java/com/android/example/roomwordssample/SetTimeActivity.java
+++ app/src/main/java/com/android/example/roomwordssample/SetTimeActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import com.android.example.dcnlab.R;
 
 public class SetTimeActivity extends AppCompatActivity {
 
